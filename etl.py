581f266ddf7d51cee869d4bd6fd210cd7c33d146
cftg2008 <78665316+cftg2008@users.noreply.github.com>
2021-03-15 15:45:17 -0400

    Add files via upload

--- etl.py
+++ etl.py
@@ -16,18 +16,18 @@ def process_song_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0].tolist()
+    song_data = df[["song_id","title","artist_id","year","duration"]].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0].tolist()
+    artist_data = df[["artist_id","artist_name","artist_location","artist_latitude","artist_longitude"]].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     """
     - Opens the log file
-    - Turns to the NextSong page
+    - Filters records by NextSong action
     - Converts the timestamp column to datetime
     - inserts time record
     - inserts user record
@@ -37,22 +37,22 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df[(df['page'] == 'NextSong')]
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
     t = pd.to_datetime(df['ts'], unit='ms')
     df['ts'] = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    time_data = list((t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday))
-    column_labels = list(('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday'))
-    time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
+    time_data = list(zip(t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday))
+    column_labels = ('start_time','hour','day','week','month','year','weekday')
+    time_df = pd.DataFrame(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
+    user_df = df[["userId","firstName","lastName","gender","level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -102,7 +102,7 @@ def process_data(cur, conn, filepath, func):
 
 def main():
     """
-    - establishes connection sparkify database and gets cursor
+    - establishes sparkify database connection and gets cursor
     - extracts, processes, and loads song data and log data
     - closes database connection
     """
@@ -110,11 +110,11 @@ def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur,conn,filepath='data/song_data',func=process_song_file)
+    process_data(cur,conn,filepath='data/log_data',func=process_log_file)
 
     conn.close()
 
-
+    
 if __name__ == "__main__":
     main()
